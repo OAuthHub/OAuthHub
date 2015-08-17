69d85b76ec36ea3646602a495b8aa0376fe3be95
Eason Chen <easoncxz@gmail.com>
2015-08-18 01:40:34 +1200

    Demonstrated ForeignKey relationship.
    Tested (manually) INSERT to work.

--- models.py
+++ models.py
@@ -16,25 +16,41 @@ class Consumer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String(300))
     secret = db.Column(db.String(600))
-    #user_accesses = db.relationship('ConsumerUserAccess',
-    #        backref=db.backref('config'), lazy='dynamic')
+    user_accesses = db.relationship('ConsumerUserAccess',
+            backref=db.backref('consumer'), lazy='dynamic')
+
+    def __init__(self, key=None, secret=None):
+        self.key = key
+        self.secret = secret
 
     def __repr__(self):
         return "<Consumer(id='{}', key='{}', secret='{}')>".format(
-            self.id, self.key, self.secret)
+                self.id, self.key, self.secret)
 
-#class AccessToken(db.Model):
-#    __tablename__ = 'access_tokens'
-#
-#    id = db.Column(db.Integer, primary_key=True)
-#    token = db.Column(db.String)
-#    secret = db.Column(db.String)
-#    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'))
-#
-#    def __repr__(self):
-#        return "<AccessToken(user='{}', server='{}', token='{}', secret='{}')>".format(self.user.name, self.server.name, self.token, self.secret)
-#
+class ConsumerUserAccess(db.Model):
+    __tablename__ = 'consumer_user_access'
+
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(1000))
+    secret = db.Column(db.String(2000))
+    consumer_id = db.Column(db.Integer, db.ForeignKey('consumer.id'))
+
+    def __init__(self, token=None, secret=None):
+        self.token = token
+        self.secret = secret
+
+    def __repr__(self):
+        return ("<ConsumerUserAccess(id='{}', token='{}', " +
+                "secret='{}', consumer='{}'), user='{}'>").format(
+                self.id, self.token, self.secret, self.consumer, '@@@')
+
+def do():
+    ''' Save some typing in the REPL.
+    '''
+    c = Consumer('CK', 'CS')
+    a = ConsumerUserAccess('AT', 'AS')
+    return c, a
+
 #class OAuthServer(db.Model):
 #    __tablename__ = 'servers'
 #
