5945322952f1397a4eccabe326c22aee59627596
Eason Chen <easoncxz@gmail.com>
2015-08-18 01:18:39 +1200

    Created simple model. Tested to work.
    Environment:
    
    - An env var for the SQL URI string;
    - New MySQL engine: PyMySQL; and
    - MySQL running.

--- models.py
+++ models.py
@@ -1,36 +1,45 @@
+import os
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
 
-class User(db.Model):
-    __tablename__ = 'users'
+# http://stackoverflow.com/questions/22252397/importerror-no-module-named-mysqldb
+# http://stackoverflow.com/questions/2952187/getting-error-loading-mysqldb-module-no-module-named-mysqldb-have-tried-pre
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    fullname = db.Column(db.String)
-    password = db.Column(db.String)
-    authorizations = db.relationship("AccessToken", backref=db.backref("user"))
+db = SQLAlchemy(app)
 
-    def __repr__(self):
-        return "<User(name='{}', fullname='{}', password='{}')>".format(self.name, self.fullname, self.password)
-
-class AccessToken(db.Model):
-    __tablename__ = 'access_tokens'
+class Consumer(db.Model):
+    __tablename__ = 'consumer'
 
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String)
-    secret = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'))
+    key = db.Column(db.String(300))
+    secret = db.Column(db.String(600))
+    #user_accesses = db.relationship('ConsumerUserAccess',
+    #        backref=db.backref('config'), lazy='dynamic')
 
     def __repr__(self):
-        return "<AccessToken(user='{}', server='{}', token='{}', secret='{}')>".format(self.user.name, self.server.name, self.token, self.secret)
+        return "<Consumer(id='{}', key='{}', secret='{}')>".format(
+            self.id, self.key, self.secret)
 
-class OAuthServer(db.Model):
-    __tablename__ = 'servers'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-
-    def __repr__(self):
-        return "<OAuthServer(name='{}')>".format(self.name)
+#class AccessToken(db.Model):
+#    __tablename__ = 'access_tokens'
+#
+#    id = db.Column(db.Integer, primary_key=True)
+#    token = db.Column(db.String)
+#    secret = db.Column(db.String)
+#    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#    server_id = db.Column(db.Integer, db.ForeignKey('servers.id'))
+#
+#    def __repr__(self):
+#        return "<AccessToken(user='{}', server='{}', token='{}', secret='{}')>".format(self.user.name, self.server.name, self.token, self.secret)
+#
+#class OAuthServer(db.Model):
+#    __tablename__ = 'servers'
+#
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String)
+#
+#    def __repr__(self):
+#        return "<OAuthServer(name='{}')>".format(self.name)
